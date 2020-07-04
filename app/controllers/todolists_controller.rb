62ebae1439808ed77433dc60b1b886c4d2a5d09d
上田智也 <tomo@uedatomoyanoMacBook-Pro.local>
2020-07-05 01:26:08 +0900

    3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,7 +2,14 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
-
+  def create
+    # ストロングパラメーターを使用
+    list = List.new(list_params)
+    # DBへ保存する
+    list.save
+    # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
   def index
   	@lists = List.all
   end
@@ -17,10 +24,15 @@ class TodolistsController < ApplicationController
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+  def destroy
+    list = List.find(params[:id]) #データ(レコード)を1件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to todolists_path #List一覧画面へリダイレクト
+  end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f16b8132ebbc46f4d726627a27bdf32e94b5ba8cef62fc63730cfa91d97b2e73175108b67b60dd7ac73c070135c35390c3d65acb0bdaeea9aeea2f6bde8c88f9'
