93009163e38b6370dc664fa3a1fd238654b2027b
上田智也 <tomo@uedatomoyanoMacBook-Pro.local>
2020-07-05 14:27:23 +0900

    3-11

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
-  get 'top' => 'homes#top'
+  get 'top' => 'homes#top' #トップページ
  
-  post 'todolists' => 'todolists#create'
+  post 'todolists' => 'todolists#create' #データ登録
 
-  get 'todolists' => 'todolists#index'
+  get 'todolists' => 'todolists#index' #一覧表示
 
-  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist' #詳細表示
 
-  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist' #編集
 
-  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist' #編集の保存
 
-  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist' #トップページ
 end
